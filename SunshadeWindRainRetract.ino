30b6e097452119a04761ea97d643bc3426f356a7
Kees Guijt <kees.guijt@upcmail.nl>
2017-01-28 20:41:14 +0100

    Rename main file

--- SunshadeWindRainRetract.ino
+++ SunshadeWindRainRetract.ino
@@ -522,6 +522,158 @@ time_t requestSync()
 
 
 
+
+
+/*   This sketch allows you to emulate a Somfy RTS or Simu HZ remote.
+   If you want to learn more about the Somfy RTS protocol, check out https://pushstack.wordpress.com/somfy-rts-protocol/
+   
+   The rolling code will be stored in EEPROM, so that you can power the Arduino off.
+   
+   Easiest way to make it work for you:
+    - Choose a remote number
+    - Choose a starting point for the rolling code. Any unsigned int works, 1 is a good start
+    - Upload the sketch
+    - Long-press the program button of YOUR ACTUAL REMOTE until your blind goes up and down slightly
+    - send 'p' to the serial terminal
+    - me: your blind should go up and down slightly
+  To make a group command, just repeat the last two steps with another blind (one by one)
+  
+  Then:
+    - m, u or h will make it to go up
+    - s make it stop
+    - b, or d will make it to go down
+    - you can also send a HEX number directly for any weird command you (0x9 for the sun and wind detector for instance)
+*/
+
+#include <EEPROM.h>
+#define PORT_RF 3 // DigitalPin 
+
+#define SYMBOL 640
+#define HAUT 0x2
+#define STOP 0x1
+#define BAS 0x4
+#define PROG 0x8
+#define EEPROM_ADDRESS 0
+
+#define REMOTE 0x121300    //<-- Change it!
+
+unsigned int newRollingCode = 101;       //<-- Change it!
+unsigned int rollingCode = 0;
+byte frame[7];
+byte checksum;
+
+void BuildFrame(byte *frame, byte button);
+void SendCommand(byte *frame, byte sync);
+
+
+void BuildFrame(byte *frame, byte button) {
+  unsigned int code;
+  EEPROM.get(EEPROM_ADDRESS, code);
+  frame[0] = 0xA7; // Encryption key. Doesn't matter much
+  frame[1] = button << 4;  // Which button did  you press? The 4 LSB will be the checksum
+  frame[2] = code >> 8;    // Rolling code (big endian)
+  frame[3] = code;         // Rolling code
+  frame[4] = REMOTE >> 16; // Remote address
+  frame[5] = REMOTE >>  8; // Remote address
+  frame[6] = REMOTE;       // Remote address
+
+  Serial.print("Frame         : ");
+  for(byte i = 0; i < 7; i++) {
+    if(frame[i] >> 4 == 0) { //  Displays leading zero in case the most significant
+      Serial.print("0");     // nibble is a 0.
+    }
+    Serial.print(frame[i],HEX); Serial.print(" ");
+  }
+  
+// Checksum calculation: a XOR of all the nibbles
+  checksum = 0;
+  for(byte i = 0; i < 7; i++) {
+    checksum = checksum ^ frame[i] ^ (frame[i] >> 4);
+  }
+  checksum &= 0b1111; // We keep the last 4 bits only
+
+
+//Checksum integration
+  frame[1] |= checksum; //  If a XOR of all the nibbles is equal to 0, the blinds will
+                        // consider the checksum ok.
+
+  Serial.println(""); Serial.print("With checksum : ");
+  for(byte i = 0; i < 7; i++) {
+    if(frame[i] >> 4 == 0) {
+      Serial.print("0");
+    }
+    Serial.print(frame[i],HEX); Serial.print(" ");
+  }
+
+  
+// Obfuscation: a XOR of all the bytes
+  for(byte i = 1; i < 7; i++) {
+    frame[i] ^= frame[i-1];
+  }
+
+  Serial.println(""); Serial.print("Obfuscated    : ");
+  for(byte i = 0; i < 7; i++) {
+    if(frame[i] >> 4 == 0) {
+      Serial.print("0");
+    }
+    Serial.print(frame[i],HEX); Serial.print(" ");
+  }
+  Serial.println("");
+  Serial.print("Rolling Code  : "); Serial.println(code);
+  EEPROM.put(EEPROM_ADDRESS, code + 1); //  We store the value of the rolling code in the
+                                        // EEPROM. It should take up to 2 adresses but the
+                                        // Arduino function takes care of it.
+}
+
+void SendCommand(byte *frame, byte sync) {
+  if(sync == 2) { // Only with the first frame.
+  //Wake-up pulse & Silence
+    digitalWrite(PORT_RF, 1);
+    delayMicroseconds(9415);
+    digitalWrite(PORT_RF, 0);
+    delayMicroseconds(89565);
+  }
+
+// Hardware sync: two sync for the first frame, seven for the following ones.
+  for (int i = 0; i < sync; i++) {
+    digitalWrite(PORT_RF, 1);
+    delayMicroseconds(4*SYMBOL);
+    digitalWrite(PORT_RF, 0);
+    delayMicroseconds(4*SYMBOL);
+  }
+
+// Software sync
+  digitalWrite(PORT_RF, 1);
+  delayMicroseconds(4550);
+  digitalWrite(PORT_RF, 0);
+  delayMicroseconds(SYMBOL);
+  
+  
+//Data: bits are sent one by one, starting with the MSB.
+  for(byte i = 0; i < 56; i++) {
+    if(((frame[i/8] >> (7 - (i%8))) & 1) == 1) {
+      digitalWrite(PORT_RF, 0);
+      delayMicroseconds(SYMBOL);
+      digitalWrite(PORT_RF, 1);
+      delayMicroseconds(SYMBOL);
+    }
+    else {
+      digitalWrite(PORT_RF, 1);
+      delayMicroseconds(SYMBOL);
+      digitalWrite(PORT_RF, 0);
+      delayMicroseconds(SYMBOL);
+    }
+  }
+  
+  digitalWrite(PORT_RF, 0);
+  delayMicroseconds(30415); // Inter-frame silence
+}
+
+
+
+
+
+
 void setup () {
 	Serial.begin(9600);
  	Serial.print("[WR]\n");
@@ -560,26 +712,39 @@ setSyncProvider( requestSync);	//set function to call when sync required
 Serial.println("Waiting for sync message");
 
 
+
+
+//Somfy
+  pinMode(PORT_RF, OUTPUT);  //Pin  an output
+  digitalWrite(PORT_RF, 0); // Pin  LOW
+
+  if (EEPROM.get(EEPROM_ADDRESS, rollingCode) < newRollingCode) {
+    EEPROM.put(EEPROM_ADDRESS, newRollingCode);
+  }
+  Serial.print("Simulated remote number : "); Serial.println(REMOTE, HEX);
+  Serial.print("Current rolling code    : "); Serial.println(rollingCode);
+
+
+
 }
 
 
 void loop () {
   
+    if (Serial.available()) {
+      processSyncMessage();
+    }
+
 	cli();
 	word p = pulse;
 	pulse = 0;
 	sei();
 
 	int secondOld=0;
-
+	
 	if (p != 0) {
 	//Serial.println("Waiting for sync message");
- 
-
-    if (Serial.available()) {
-      processSyncMessage();
-    }
-
+	
 		if (orscV2.nextPulse(p))
 			reportSerial("OSV2", orscV2);  
 		if (ventus.nextPulse(p))
